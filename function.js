dca1e306ef87f72b81055bec882c119757f65ce3
Aman Jain <112370476+Itsamanjain@users.noreply.github.com>
2023-07-26 18:14:08 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,92 @@
+// function,alerts,loops in javascript
+
+// console.log("learning fynction");
+
+// function greet(name){
+//     console.log(name + " is a good boy");
+
+// }
+
+// function sum(a,b,c){
+//     return a+b+c;
+// }
+
+// let name = "aman";
+// let name1 = "rahl";
+// let name2 = "devta";
+// let name3 = "jiwaji";
+
+// greet(name);
+// greet(name1);
+// greet(name2);
+// greet(name3);
+
+// console.log(sum(2,2,34));
+
+
+// alert, prompt,confirm
+
+// alert("thanks for visiting the website");
+
+// let age = prompt("enter your age ","age");
+
+// if(age>=18){
+//     alert("you are eligble for the party");
+// }else{
+//     alert("you are not eligible for the party");
+// }
+
+//confirm("are you sure");
+
+
+        //loops to iterarte array
+// let friends = ['aman','gunu','nishu'];
+// for (let index = 0; index < friends.length; index++) {
+//     // const element = array[index];
+//     console.log(index);
+    
+// }
+
+
+// friends.forEach(function f(element){
+//     console.log("hello " + element + " to modern js");
+// })
+
+
+// for(element of friends){
+//     console.log("hello " + element + " to modern javascript");   
+// }
+
+
+    // to iterarate objects
+
+    // let nokar = {
+    //     nam: "aman",
+    //     Kam: 6,
+    //     channel: "sb"
+    // }
+
+// use this  loop to iterate over objects in js
+
+    // for(key in nokar){
+    //     console.log(`the ${key} of nokar is ${nokar[key]}`);
+    // }
+
+    // while loop in js
+
+
+    //let k = 0;
+    // while (k<5) {
+    //     console.log(`${k} is less than 5`);
+    //     k++;
+        
+    // }
+
+
+    // do while loop
+
+    // do
+    // {
+    //     console.log(`${k} is less than 5`);
+    //     k++;
+    // }while(k<5); 
