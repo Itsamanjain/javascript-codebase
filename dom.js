c43119494b1502d9617d036a7b2a0746785f21dd
Itsamanjain <112370476+Itsamanjain@users.noreply.github.com>
2023-07-28 18:47:34 +0530

    second

---
+++ dom.js
@@ -0,0 +1,14 @@
+// dom manipulation 
+
+// access dom elements -->
+
+// getElementbyid
+// getelementbyclassname
+// getelementbytagname
+
+// queryselector
+// queryselectorAll
+
+// changing content 
+// create and appending elements
+//event handling
